e616b1bc0a137b7ff4760fbb940c56ea1d3acf29
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 14:55:46 +0200

    subscribe to ModConfigEvent

--- src/main/java/dk/magnusjensen/oregenmod/util/ModEventBusSubscriber.java
+++ src/main/java/dk/magnusjensen/oregenmod/util/ModEventBusSubscriber.java
@@ -1,2 +1,20 @@
-package dk.magnusjensen.oregenmod.util;public class ModEventBusSubscriber {
+package dk.magnusjensen.oregenmod.util;
+
+import dk.magnusjensen.oregenmod.OreGenMod;
+import dk.magnusjensen.oregenmod.config.ConfigHelper;
+import dk.magnusjensen.oregenmod.config.ConfigHolder;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
+@Mod.EventBusSubscriber(modid = OreGenMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusSubscriber {
+
+	@SubscribeEvent
+	public static void onModConfigEvent(ModConfig.ModConfigEvent event) {
+		ModConfig config = event.getConfig();
+		if (config.getSpec() == ConfigHolder.SERVER_SPEC) {
+			ConfigHelper.bakeServer(config);
+		}
+	}
 }

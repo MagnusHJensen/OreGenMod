142faf6fc651cda999e1027ca67c5166f3858267
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 15:05:11 +0200

    setup the server config instance, and server_spec

--- src/main/java/dk/magnusjensen/oregenmod/config/ConfigHolder.java
+++ src/main/java/dk/magnusjensen/oregenmod/config/ConfigHolder.java
@@ -1,2 +1,17 @@
-package dk.magnusjensen.oregenmod.config;public class ConfigHolder {
+package dk.magnusjensen.oregenmod.config;
+
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class ConfigHolder {
+
+	public static final ForgeConfigSpec SERVER_SPEC;
+
+	static final ServerConfig SERVER;
+	static {
+		final Pair<ServerConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
+		SERVER = specPair.getLeft();
+		SERVER_SPEC = specPair.getRight();
+	}
 }

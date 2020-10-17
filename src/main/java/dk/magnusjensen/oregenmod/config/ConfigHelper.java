6f690ce8118e8573873bf3387ad272fa1f18540e
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 15:22:34 +0200

    We need to Cast the Config types, a lot to access the different fields.

--- src/main/java/dk/magnusjensen/oregenmod/config/ConfigHelper.java
+++ src/main/java/dk/magnusjensen/oregenmod/config/ConfigHelper.java
@@ -1,2 +1,24 @@
-package dk.magnusjensen.oregenmod.config;public class ConfigHelper {
+package dk.magnusjensen.oregenmod.config;
+
+import dk.magnusjensen.oregenmod.world.gen.OreGeneration;
+import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
+import net.minecraft.world.gen.feature.FeatureSpreadConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.DecoratedPlacementConfig;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
+import net.minecraftforge.fml.config.ModConfig;
+
+public class ConfigHelper {
+
+	public static void bakeServer(final ModConfig config) {
+
+		// Vein Size
+		((OreFeatureConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) OreGeneration.AMETHYST_ORE.config).feature.get().config).feature.get().config).size = ConfigHolder.SERVER.serverAmethystVeinSize.get();
+		// Vein Count
+		((FeatureSpreadConfig)((DecoratedFeatureConfig) OreGeneration.AMETHYST_ORE.config).decorator.config).func_242799_a().field_242250_b = ConfigHolder.SERVER.serverAmethystVeinCount.get();
+		// Minimum Height the ore can spawn
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) OreGeneration.AMETHYST_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242813_c = ConfigHolder.SERVER.serverAmethystBottomHeight.get();
+		// Maximum Height the ore can spawn.
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) OreGeneration.AMETHYST_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242815_e = ConfigHolder.SERVER.serverAmethystMaxHeight.get();
+	}
 }

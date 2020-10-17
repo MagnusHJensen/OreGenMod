e4bc93685cf02df126507cdbfb7b3137c3c194e3
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 15:02:43 +0200

    subscribe to the biomeloading event

--- src/main/java/dk/magnusjensen/oregenmod/util/ForgeEventBusSubscriber.java
+++ src/main/java/dk/magnusjensen/oregenmod/util/ForgeEventBusSubscriber.java
@@ -1,2 +1,21 @@
-package dk.magnusjensen.oregenmod.util;public class ForgeEventBusSubscriber {
+package dk.magnusjensen.oregenmod.util;
+
+import dk.magnusjensen.oregenmod.OreGenMod;
+import dk.magnusjensen.oregenmod.world.gen.OreGeneration;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OreGenMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ForgeEventBusSubscriber {
+
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	public static void onBiomeLoading(BiomeLoadingEvent event) {
+		if (event.getCategory() != Biome.Category.NETHER && event.getCategory() != Biome.Category.THEEND) {
+			OreGeneration.initOverWorldFeatures();
+			OreGeneration.generateOverworldOres(event);
+		}
+	}
 }

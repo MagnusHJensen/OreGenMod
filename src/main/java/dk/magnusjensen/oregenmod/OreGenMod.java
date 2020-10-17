b8280c2a331edab4256aa53708a98bfeb5c80bfc
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 14:54:02 +0200

    fixed imports

--- src/main/java/dk/magnusjensen/oregenmod/OreGenMod.java
+++ src/main/java/dk/magnusjensen/oregenmod/OreGenMod.java
@@ -1,27 +1,20 @@
 package dk.magnusjensen.oregenmod;
 
 import dk.magnusjensen.oregenmod.config.ConfigHolder;
-import net.minecraft.block.Block;
+import dk.magnusjensen.oregenmod.util.RegistryHandler;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.rmi.registry.RegistryHandler;
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("oregenmod")
